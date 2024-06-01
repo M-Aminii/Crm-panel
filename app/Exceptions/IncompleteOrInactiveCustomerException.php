bffd58557e204f7709451212f5a868ad54684608
MostafaAmini <mostafaaminizadeh@gmail.com>
2024-06-01 14:09:04 +0330

    add calculatePriceDoubleLaminate to InvoiceServic 2

---
+++ app/Exceptions/IncompleteOrInactiveCustomerException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class IncompleteOrInactiveCustomerException extends Exception
+{
+    public function render($request)
+    {
+        return response()->json(['message' => 'امکان صدور فاکتور برای این مشتری وجود ندارد'], 401);
+    }
+}
